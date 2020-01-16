c7ab7e0509cb42ef1d49633ebd633aff0eb1a4b6
namvuong97 <55384686+namvuong97@users.noreply.github.com>
2020-01-16 23:26:29 +0700

    fruit

--- fruit/js/script.js
+++ fruit/js/script.js
@@ -1,1 +1,4 @@
-$('.main').load('../header.html')
+$('#header').load('../header.html')
+$('#footer').load('../footer.html');
+// $("#back-to-top").click(function(){return $("body, html").animate({scrollTop:0},400),!1});
+// $(function(){$('[data-toggle="tooltip"]').tooltip()});
